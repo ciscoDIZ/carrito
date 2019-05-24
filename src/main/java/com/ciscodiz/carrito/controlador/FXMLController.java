cf51d71ecd92d3964e776f784fcd48b9d79573a8
tote <tote@tote-MS-7B86>
2019-05-24 13:49:52 +0100

    comenzando controlador

--- src/main/java/com/ciscodiz/carrito/controlador/FXMLController.java
+++ src/main/java/com/ciscodiz/carrito/controlador/FXMLController.java
@@ -6,13 +6,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class FXMLController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private TextField txtId;
+    @FXML
+    private TextField txtPrecio;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtCantidad;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -22,4 +30,8 @@ public class FXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btnAgregarOnClick(MouseEvent event) {
+    }
 }
